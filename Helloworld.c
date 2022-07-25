f38ea585bab65f8cabefad2f9ca6759f85069e0b
Niol Li <Niol_li@amat.com>
2022-07-25 13:20:25 +0800

    add more print out

--- Helloworld.c
+++ Helloworld.c
@@ -4,5 +4,12 @@ int main()
 {
 
  printf("Hello, World!");
+ printf("Hello, World 1!");
+ printf("Hello, World 2!");
+ printf("Hello, World 3!");
+ printf("Hello, World 4!");
+ printf("Hello, World 5!");
+ printf("Hello, World 6!");
+ printf("Hello, World 7!");
  return 0;
 }
